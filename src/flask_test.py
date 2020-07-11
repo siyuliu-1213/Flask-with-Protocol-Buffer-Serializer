da473dcb1b8cba96c30c19d24a0a776eeeee2d75
Siyu Liu <55250584+siyuliu-1213@users.noreply.github.com>
2020-07-11 20:23:53 +0800

    add static folder

--- src/flask_test.py
+++ src/flask_test.py
@@ -60,7 +60,7 @@ def test():
 
 @app.route('/')
 def index():
-    return render_template('home.html', name=name)
+    return render_template('home.html')
 
 if __name__=='__main__':
     app.run(debug=True)
